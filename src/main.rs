ad95325349818d685674cbf54a3e05fe78dc924b
Jakub Żuchowski <ellmunzai@gmail.com>
2024-09-29 19:56:23 +0200

    using std::io to read keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-  println!("dupa");
+  println!("Type me something:");
+
+  //mutable variable (has to be in order to record keyboard input)
+  let mut something = String::new();
+
+  io::stdin()
+    .read_line(&mut something)
+    .expect("-read line fail-");
+
+  println!("You gave me: {}", something);
 }
